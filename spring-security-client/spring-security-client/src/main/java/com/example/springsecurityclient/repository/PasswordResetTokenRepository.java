95152b64abb09834a1bd8515edc525857a1ad5fc
abinayas <abinayas@codegen.net>
2024-04-30 11:57:18 +0530

    first commit

---
+++ spring-security-client/spring-security-client/src/main/java/com/example/springsecurityclient/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityclient.repository;
+
+import com.example.springsecurityclient.entity.PasswordResetToken;
+import com.example.springsecurityclient.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken,Long>
+{
+
+    PasswordResetToken findByToken( String token );
+}
